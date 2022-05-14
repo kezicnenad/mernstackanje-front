d3a7fb3725c6acc5e26a244d0745f9d0144b98d5
Kežić Nenad <kezicnenad@gmail.com>
2022-05-14 15:11:26 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+
+  // const URI = "https://mern-stack-crud-server.herokuapp.com";
+  const URI = "http://localhost:5000";
+
+  const fetchApi = () => {
+    fetch(URI + "/services/add", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'charset': "utf-8",
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        name: "Name",
+        description: "Opis",
+        logo: "Slika",
+        link: "Link",
+      }),
+    });
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="jumbotron p-5 bg-light">
+      <h1>App</h1>
+      <button type="button" className="btn btn-primary" onClick={()=>fetchApi()}>Post service</button>
     </div>
   );
 }
